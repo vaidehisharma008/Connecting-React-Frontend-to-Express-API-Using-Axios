32a0994947f7b3de02638d6a82d80b8d5c0ae027
Vaidehi Sharma <vaidehisharma703@gmail.com>
2025-10-24 22:10:43 +0530

    Create main App component for product store

---
+++ frontend app.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ProductList from "./ProductList";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🛍️ Product Store</h1>
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
